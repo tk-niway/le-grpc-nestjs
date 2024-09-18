fc425d5337b043c6e5085c7991409a5eab1225d2
tk-niway <urban.hang274@gmail.com>
2024-09-18 15:42:02 +0900

    implemented interceptor with gRPC for client

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -4,7 +4,41 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReflectionService } from '@grpc/reflection';
+import {
+  InterceptingCall,
+} from '@grpc/grpc-js';
 
+// gRPC interceptorはクライアントのみで利用可能
+function interceptor(options: any, nextCall: any) {
+  console.log('Intercepting call');
+  const interceptingCall = new InterceptingCall(nextCall(options), {
+    start: function (metadata, _listener, next) {
+      const response: any = {};
+
+      const listener = {
+        onReceiveMetadata: (metadata: any, next: any) => {
+          console.log('Received metadata:', metadata);
+          response.metadata = metadata;
+          next(metadata);
+        },
+        onReceiveMessage: (message: any, next: any) => {
+          console.log('Received message:', message);
+          response.message = message;
+          next(response);
+        },
+        onReceiveStatus: (status: any, next: any) => {
+          console.log('Received status:', status);
+          const { code, details } = status;
+          response.status = { code, details };
+          next(status);
+        },
+      };
+
+      next(metadata, listener);
+    },
+  });
+  return interceptingCall;
+}
 @Module({
   imports: [
     ClientsModule.register([
@@ -18,6 +52,9 @@ import { ReflectionService } from '@grpc/reflection';
             join(__dirname, 'proto/sample.proto'),
             join(__dirname, 'proto/hero.proto'),
           ],
+          channelOptions: {
+            interceptors: [interceptor],
+          },
           onLoadPackageDefinition: (pkg, server) => {
             new ReflectionService(pkg).addToServer(server);
           },
